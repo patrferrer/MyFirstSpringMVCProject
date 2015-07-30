7f5f8027880e0db4296cabdc5d8be9bdc6800248
lrenderos <lorena.renderos@creativaconsultores.com>
2015-07-30 07:52:18 -0600

    first commit

---
+++ src/main/java/com/test/controller/PersonRestController.java
@@ -0,0 +1,49 @@
+package com.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import com.test.model.Person;
+import com.test.service.PersonService;
+
+@RestController
+public class PersonRestController {
+
+    private PersonService personService;
+
+	public PersonService getPersonService() {
+		return personService;
+	}
+
+    @Autowired(required=true)
+    @Qualifier(value="personService")
+	public void setPersonService(PersonService personService) {
+		this.personService = personService;
+	}
+
+	@RequestMapping(value = "/api/person/{id}", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> personForm(@PathVariable int id) {
+		Person person = personService.getPersonById(id);
+		System.out.println(person);
+		return new ResponseEntity<>(person, null, HttpStatus.FOUND);
+	}
+
+	@RequestMapping(value = "/api/person", method = RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> personSave(@RequestBody Person person) {
+		personService.addPerson(person);
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setLocation(
+				ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(person.getId()).toUri());
+		return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
+	}
+}
---
+++ src/main/java/com/test/service/PersonServiceImpl.java
@@ -0,0 +1,50 @@
+package com.test.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+import com.test.dao.PersonDAO;
+import com.test.model.Person;
+
+public class PersonServiceImpl implements PersonService {
+     
+    private PersonDAO personDAO;
+
+    public void setPersonDAO(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+ 
+    @Override
+    @Transactional
+    public void addPerson(Person p) {
+        this.personDAO.addPerson(p);
+    }
+ 
+    @Override
+    @Transactional
+    public void updatePerson(Person p) {
+        this.personDAO.updatePerson(p);
+    }
+ 
+    @Override
+    @Transactional
+    public List<Person> listPersons() {
+        return this.personDAO.listPersons();
+    }
+ 
+    @Override
+    @Transactional
+    public Person getPersonById(int id) {
+        return this.personDAO.getPersonById(id);
+    }
+ 
+    @Override
+    @Transactional
+    public void removePerson(int id) {
+        this.personDAO.removePerson(id);
+    }
+ 
+}
